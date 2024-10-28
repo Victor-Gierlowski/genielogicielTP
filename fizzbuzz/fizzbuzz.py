140c2d1990ae22ebe4e6f8f2070551506c4da98c
vgierlowski <victor.gierlowski@etu.unistra.fr>
2024-10-28 13:30:23 +0100

    FizzBuzz impleted

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,4 +1,11 @@
 class FizzBuzz:
     def convert(self, number):
-        raise NotImplementedError
+        out = ""
+        if(number % 3 == 0):
+            out += "Fizz"
+        if(number % 5 == 0):
+            out += "Buzz"
+        if(out == ""):
+            out = str(number)
+        return out
 
